aa460ef87201cd10219ae66905f673ccd2a2b34d
amlbuamer2001 <devaml2001@gmail.com>
2025-12-01 10:31:24 +0200

    refactor: improve post rendering and enhance layout in Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,19 +21,17 @@ export default function Home() {
 {posts.map((post)=>{
   return(
     <div key={post.id} className="w-full md:w-[80%] lg:w-[60%] mx-auto my-8 p-4 rounded-md bg-slate-100">
-      <div className="flex align-center justify-between">
-        <div className="flex items-center gap-3 mb-4">
-          <img src={post.user.photo} alt={post.user.name} className="size-[40px] rounded-full" />
+      <div className="flex align-center justify-between mb-4">
+        <div className="flex items-center gap-3">
+          <img src={post.user.photo||''} alt={post.user.name} className="size-[40px] rounded-full" />
           <p className="font-bold">{post.user.name}</p>
         </div>
         <div>
-          <p>{post.createdAt}</p>
+          <p className="text-xs text-slate-400">{post.createdAt}</p>
         </div>
       </div>
-      <h2 className="mb-4">
-        {post.body}
-      </h2>
-      <img src={post.image} alt={post.body} className="w-full rounded-md" />
+     {post.body && <p className="mb-4">{post.body}</p>}
+{ post.image && <img src={post.image} alt={post.body} className="w-full max-h-[400px] object-cover rounded-md" />  }
     </div>
   )
 })}
--- src/Context/PostsContext.jsx
+++ src/Context/PostsContext.jsx
@@ -4,6 +4,8 @@ import { createContext } from "react";
 export const PostsContext = createContext();
 
 export function PostsContextProvider(props) {
+
+  // عملنا فانكشن في الكونتكست عشان لو احتجناها في مكان ثاني
   function getAllPosts() {
     // fetch all posts from the server
     return axios.get(`https://linked-posts.routemisr.com/posts?limit=50`, {
